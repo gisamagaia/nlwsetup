d9e8a97dc4bfa9c6e7e88d1f18c9574a57e4717b
Giovanna Samagaia <giovanna.reboucas@hotmail.com>
2023-04-10 00:56:36 -0300

    desenvolvimento do nlw setup

---
+++ script.js
@@ -0,0 +1,21 @@
+// declarar e atribuir o valor se usa Let 
+//et boasvindas = "Olá face!"
+
+// reatribuição de valor 
+//boasvindas= "Oie, tudo bem?"
+
+//constante não pode mudar valor
+//const serhumano = true
+// não adianta colocar false pq vai dar erro 
+
+//const celular = {
+  //  cor: 'preto',
+   // ligar: function() {
+    //    const mensagem = "Ligando"
+    //    alert(mensagem)
+   // }
+//}
+
+//celular.ligar()
+
+document.querySelector
